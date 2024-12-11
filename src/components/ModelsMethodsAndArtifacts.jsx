75d25d112cca7ee78c0870fac1e35dab11bd2648
Divyesh Jani <djani03@gmail.com>
2024-12-11 15:50:06 -0500

    Adding content and resources for Models, Methods, and Artifacts

--- src/components/ModelsMethodsAndArtifacts.jsx
+++ src/components/ModelsMethodsAndArtifacts.jsx
@@ -2,6 +2,136 @@ export default function ModelsMethodsAndArtifacts() {
   return (
     <div>
       <h2 className='text-center display-6 fw-normal'>Models, Methods, and Artifacts</h2>
+
+      <h4>Tailoring</h4>
+      <p>Tailoring is the deliberate adaptation of project management approach, governance, and processes to make them more suitable for the given environment and the work at hand. Tailoring involves understanding the project context, goals, and operating environment.</p>
+      <p>Tailoring that only impacts the project team requires less oversight than tailoring that impacts external groups, therefore internal project tailoring might be approved by the project manager while tailoring changes that impact external groups may require approval by the Project Management Office (PMO) or Value Delivery Office (VDO).</p>
+      <p>VDO may be found in organizations that use more adaptive delivery approaches. The VDO serves an enabling role, rather than a management or oversight function.</p>
+      <figure className='figure'>
+        <img src='./src/assets/models/tailoring.png' className='figure-img img-fluid rounded' alt='Tailoring Suggestions for Situations' />
+      </figure>
+
+      <h4>Models, Methods, and Artifacts</h4>
+      <p><strong>Model</strong> is a thinking strategy to explain a process, framework, or phenomenon.</p>
+      <p><strong>Method</strong> is the means for achieving an outcome, output, result, or project deliverable.</p>
+      <p><strong>Artifact</strong> can be a template, document, output, or project deliverable.</p>
+
+      <h6>Situational Leadership Models:</h6>
+      <ul>
+        <li>Ken Blanchard's Situational Leadership II</li>
+        <ul>
+          <li>Measures team member development using competence and commitment as two main variables</li>
+          <li>Competence is combination of ability, knowledge, and skill</li>
+          <li>Commitment is the confidence and motivation an individual has</li>
+        </ul>
+        <li>OSCAR Model developed by Karen Whittleworth and Andrew Gilbert</li>
+        <ul>
+          <li>Outcome (identifies long-term goal and desired result)</li>
+          <li>Situation (enables conversation about current skills, knowledge level)</li>
+          <li>Choices/Consequences (identify potential avenues for attaining desired outcomes)</li>
+          <li>Actions (commit to specific improvements by focusing on target)</li>
+          <li>Review (regular meetings to support and help remain motivated)</li>
+        </ul>
+      </ul>
+      <p>Donald Norman described the Guld of Execution as the degree to which an item corresponds with what a person expects it to do.</p>
+
+      <h6>Hygiene and Motivational Factors</h6>
+      <p>Frederick Herzberg, based on study, believed that job satisfaction and dissatisfaction stem from conditions called motivational factors. Motivational factors include matters that relate to the content of the work, such as achievement, growth, and advancement. Insufficient motivational factors lead to dissatisfaction. Sufficient motivational factors lead to satisfaction.</p>
+      <p>Herzberg also identified Hygiene Factors related to work, such as company policies, salary, and physical environment. If hygiene factors are insufficient, they cause dissatisfaction. However, even if they are sufficient, they do not lead to satisfaction.</p>
+
+      <h6>Intrinsic versus Extrinsic Motivation</h6>
+      <p>Daniel Pink stated that while extrinsic rewards, such as salary, are motivators to a certain extent, once a person is paid fairly, the motivational power of extrinsic rewards ceases to exist. For complicated and challenging work, intrinsic motivators are far longer lasting and more effective. Three intrinsic motivators:</p>
+      <ul>
+        <li>Autonomy (desire to direct one's own life, aligned with being able to determine how, when, and where to work)</li>
+        <li>Mastery (being able to improve and excel)</li>
+        <li>Purpose (need to make a difference, knowing the project vision and how their work contributes to achieving that vision)</li>
+      </ul>
+
+      <h6>Theory of Needs</h6>
+      <p>David McClellan's model states that people are driven by needs of achievement, power, and affiliation. The relative strength of each need depends on individual's experience and culture.</p>
+      <ul>
+        <li>Achievement (motivated by work that is challenging but reasonable)</li>
+        <li>Power (like to organize, motivate, and lead others. Motivated by increased responsibility)</li>
+        <li>Affiliation (seek acceptance and belonging, motivated by being part of a team)</li>
+      </ul>
+
+      <h6>Theory X, Theory Y, and Theory Z</h6>
+      <p>Douglas McGregor devised Theory X and Theory Y models that represent a spectrum of employee motivation and corresponding management style. This was later expanded to include Theory Z.</p>
+      <ul>
+        <li>Theory X</li>
+        <li className='listStyleTypeNone'>Assumes individuals work for the sole purpose of income. Not ambitions or goal oriented. Corresponding management style is hands-on and top-down approach.</li>
+        <li>Theory Y</li>
+        <li className='listStyleTypeNone'>Assumes individuals are intrinsically motivated to do good work. Corresponding management style has a more personal coaching feel. Manager encourages creativity and discussion.</li>
+        <li>Theory Z</li>
+        <li className='listStyleTypeNone'>Abraham Maslow saw Theory Z as a transcendent dimension to work where individuals are motivated by self-realization, values, and higher calling. Management style is one that cultivates insight and meaning.</li>
+        <li className='listStyleTypeNone'>William Ouchi's versin of Theory Z focuses on motivating employees by focusing on well-being of employees and their families. Management style seeks to promote high productivity, morale, and satisfaction.</li>
+      </ul>
+
+      <h6>Managing Change</h6>
+      <ul>
+        <li>Formulate Change</li>
+        <li>Plan Change</li>
+        <li>Implement Change</li>
+        <li>Manage Transition</li>
+        <li>Sustain Change</li>
+      </ul>
+
+      <h6>ADKAR Model</h6>
+      <p>Jeff Hiatt developed this model which focuses on five sequential steps that individuals undergo when adapting to change:</p>
+      <ul>
+        <li>Step 1: Awareness (identifying why change is necessary)</li>
+        <li>Step 2: Desire (desire to change)</li>
+        <li>Step 3: Knowledge (understand how to change)</li>
+        <li>Step 4: Ability (supported with hands-on practice and access to expertise and help as needed)</li>
+        <li>Step 5: Reinforcement (support sustainment of change)</li>
+      </ul>
+
+      <h6>Tuckman Ladder Model for Project Team Development</h6>
+      <ul>
+        <li>Forming</li>
+        <li>Storming</li>
+        <li>Norming</li>
+        <li>Performing</li>
+        <li>Adjourning</li>
+      </ul>
+
+      <h6>Drexler/Sibbet Team Performance Model</h6>
+      <ul>
+        <li>Step 1: Orientation</li>
+        <li>Step 2: Trust Building</li>
+        <li>Step 3: Goal Clarification</li>
+        <li>Step 4: Commitment</li>
+        <li>Step 5: Implementation</li>
+        <li>Step 6: High Performance</li>
+        <li>Step 7: Renewal</li>
+      </ul>
+
+      <h6>Ken Thomas and Ralph Kilmann described six ways of addressing conflict:</h6>
+      <ul>
+        <li>Confronting/Problem solving (treat conflict as a problem to be solved)</li>
+        <li>Collaborating (use when there is trust and time to come to consensus)</li>
+        <li>Compromising (entails willingness to give and take, often used when parties involved have equal power)</li>
+        <li>Smoothing/Accommodating (used when there is difference in relative authority/power, goal more important that disagreement)</li>
+        <li>Forcing (when not enough time to problem-solve, one forces their will on the other)</li>
+        <li>Withdrawal/Avoiding (hoping problem will go away on its own)</li>
+      </ul>
+
+      <h6>Negotiation</h6>
+      <ul>
+        <li>Win-win (optimal outcome, each person is satisfied with outcome)</li>
+        <li>Win-lose/Lose-win (for someone to win, the other loses)</li>
+        <li>Lose-lose (competition overwhelms collaboration)</li>
+      </ul>
+      <p>A win-win perspective generally is found when following aspects are present: Character, Trust, and Approach.</p>
+
+      <p><strong>Decision Tree Analysis</strong> is a diagramming and calculation method for evaluating the implications of a chain of multiple options in the presence of uncertainty.</p>
+      <p><strong>Story Map</strong> is a visual model of all features and functionality desired for a given product, created to give the project team a holistic view of what they are building and why.</p>
+
+      <h6>Unique Characteristics of Project, Program, and Product</h6>
+      <figure className='figure'>
+        <img src='./src/assets/models/characteristics.png' className='figure-img img-fluid rounded' alt='Unique Characteristics of Project, Program, and Product' />
+      </figure>
+
     </div>
   );
 }
